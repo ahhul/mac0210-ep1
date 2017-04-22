6e9b0f0d39ceda17d07c1bc3793fefa2c6e9bf34
ahhul <feromagen@gmail.com>
2017-04-22 15:49:05 -0300

    add newton.m

---
+++ newton_basins.m
@@ -0,0 +1,8 @@
+# MAC0210 - EP1
+# Alunos:   Arthur Coser Marinho                NUSP: 
+#           Ludmila Ferreira Vicente e Silva    NUSP: 7557136
+
+function bla = newton_basins (f, l, u, p)
+   
+
+endfunction
