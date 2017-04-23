d9f1daf41df0fc14c3c76bf09aa0514783bb40a0
ahhul <feromagen@gmail.com>
2017-04-23 19:36:38 -0300

    consertei o role de nao convergir

--- newton.m
+++ newton.m
@@ -1,12 +1,12 @@
 # MAC0210 - EP1
-# Alunos:   Arthur Coser Marinho                NUSP: 
+# Alunos:   Arthur Coser Marinho                NUSP: 7210629
 #           Ludmila Ferreira Vicente e Silva    NUSP: 7557136
 
 function root = newton (f, df, x0)
     
     i = 0;
     err = 0;
-    lim_err = 10^-3
+    lim_err = 10^-3;
 
     do
         prev = x0;
@@ -15,10 +15,17 @@ function root = newton (f, df, x0)
         
 
         err = abs (abs (prev - abs (x0)));
-
+        
         i++;
-    until (err < lim_err)
+        
+    until (err < lim_err || i == 100)
+    
+    # Quando nao converge
+    if (err > lim_err)
+        root = 0;
+    endif
     
     root = x0;
-
+    disp(root);
+    
 endfunction
